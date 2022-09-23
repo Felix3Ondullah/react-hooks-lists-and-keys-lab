c36581d97b5df775730b77f63c90127c57d236e0
Felix Ondullah <felixondullah@gmail.com>
2022-09-23 17:19:04 +0300

    u

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,1 @@
-import React from "react";
 
-function NavBar() {
-  const links = ["home", "about", "projects"];
-
-  const navLInks = links.map((link) => 
-  <a key={link} href={"#" + link}>{link}</a>
-  )
-
-  return (
-    <nav>
-      {navLInks}
-    </nav>
-    )
-}
-
-export default NavBar;
